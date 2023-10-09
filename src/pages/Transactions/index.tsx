8748ff6d34c79ba6463b2757f5576dbd6f13a82d
Matheussmaced <mateusbyakuya3@gmail.com>
2023-10-09 20:33:24 -0300

    resolved bugs

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,17 +1,12 @@
 import { Header } from "../../components/Header"
-<<<<<<< HEAD
-=======
+
 import { Summary } from "../../components/Summary"
->>>>>>> 071cf37 (adding Summary component)
 
 export const Transactions = () => {
   return(
     <div>
         <Header />
-<<<<<<< HEAD
-=======
         <Summary />
->>>>>>> 071cf37 (adding Summary component)
     </div>
   )
 }
