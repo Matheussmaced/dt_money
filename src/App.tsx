9a0b3c3459ee652be72a86d70fddcf356aa6f829
Matheussmaced <mateusbyakuya3@gmail.com>
2023-10-09 11:37:17 -0300

    styles-components configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+    <h1>Hello World</h1>
+    </ThemeProvider>
+  )
+}
+
+export default App
