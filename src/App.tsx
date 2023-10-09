e89aa0f7c7c832fafe901d3489ba20fbb8b25b4b
Matheussmaced <mateusbyakuya3@gmail.com>
2023-10-09 11:44:49 -0300

    inicial configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyles } from "./styles/global"
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+
     <h1>Hello World</h1>
     </ThemeProvider>
   )
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,10 +5,20 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
 
-    :focus {
+  :focus {
       outline: 0;
       box-shadow: 0 0 0 2px ${props => props.theme['green-500']};
     }
+
+  body {
+    background-color: ${props => props.theme["gray-800"]};
+    color: ${props => props.theme["gray-100"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input-security, textarea, button {
+    font: 400 1rem Roboto, sans-serif;
   }
 `
