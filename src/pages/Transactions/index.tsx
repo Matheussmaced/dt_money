f79ff84ebfa25ac228f9c52f1454b8971392c951
Matheussmaced <mateusbyakuya3@gmail.com>
2023-10-09 20:15:02 -0300

    adding summary component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,17 @@
 import { Header } from "../../components/Header"
+<<<<<<< HEAD
+=======
+import { Summary } from "../../components/Summary"
+>>>>>>> 071cf37 (adding Summary component)
 
 export const Transactions = () => {
   return(
     <div>
         <Header />
+<<<<<<< HEAD
+=======
+        <Summary />
+>>>>>>> 071cf37 (adding Summary component)
     </div>
   )
 }
