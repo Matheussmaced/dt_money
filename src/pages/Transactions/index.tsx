11e2f9709fd4234a52fb50b886cb7d09f37ade21
Matheussmaced <mateusbyakuya3@gmail.com>
2023-10-13 12:04:35 -0300

    fix: formatted date and price

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -6,6 +6,7 @@ import { Summary } from "../../components/Summary"
 import { SearchForm } from "./components/SearchForm"
 import { PriceHighlight, TransactionContainer, TransactionTable } from "./styles"
 import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 
 export const Transactions = () => {
   const { transaction } = useContext(TransactionsContext)
@@ -27,11 +28,12 @@ export const Transactions = () => {
                 <td width='50%'>{transactions.description}</td>
                   <td>
                     <PriceHighlight variant={transactions.type}>
-                      {transactions.price}
+                      {transactions.type === 'outcome' && '- '}
+                      {priceFormatter.format(transactions.price)}
                     </PriceHighlight>
                   </td>
                 <td>{transactions.category}</td>
-                <td>{transactions.createdAt}</td>
+                <td>{dateFormatter.format(new Date(transactions.createdAt))}</td>
               </tr>
               )
             })}
