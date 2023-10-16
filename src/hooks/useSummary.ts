40391dd6e31a32482a790fb296c27023ac14fac7
Matheussmaced <mateusbyakuya3@gmail.com>
2023-10-16 19:10:15 -0300

    feat: adding useMemo for useSummary hook

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,30 +1,33 @@
 import { useContextSelector } from 'use-context-selector'
 
 import { TransactionsContext } from '../contexts/TransactionsContext'
+import { useMemo } from 'react'
 
 export const useSummary = () => {
   const transaction = useContextSelector(TransactionsContext, (context) => {
     return context.transaction
   })
 
-  const summary = transaction.reduce(
-    (accumulator, transactions) => {
-      if (transactions.type === 'income') {
-        accumulator.income += transactions.price
-        accumulator.total += transactions.price
-      } else {
-        accumulator.outcome += transactions.price
-        accumulator.total -= transactions.price
-      }
+  const summary = useMemo(() => {
+    transaction.reduce(
+      (accumulator, transactions) => {
+        if (transactions.type === 'income') {
+          accumulator.income += transactions.price
+          accumulator.total += transactions.price
+        } else {
+          accumulator.outcome += transactions.price
+          accumulator.total -= transactions.price
+        }
 
-      return accumulator
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return accumulator
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transaction])
 
   return summary
 }
